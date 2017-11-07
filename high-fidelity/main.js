9ce9f8d9434885154ebac77abcbc50094d7054bc
Alexander Hammonds <ahammonds247@gmail.com>
2017-11-07 15:20:12 -0600

    Sliding sidebar working

--- high-fidelity/main.js
+++ high-fidelity/main.js
@@ -1,4 +1,5 @@
 var sidebar = document.getElementById("sidebar")
+var main = document.getElementById("main")
 
 var items = [
     { link: "http://google.com", text: "Google", icon: "fa fa-th-list" },
@@ -9,7 +10,7 @@ var items = [
 
 console.log(sidebar)
 
-var opened = () => {
+var displayOpened = () => {
     var ul = document.createElement('ul')
     ul.className = "nav nav-pills flex-column";
 
@@ -30,9 +31,20 @@ var opened = () => {
         ul.appendChild(li)
         sidebar.appendChild(ul)
     })
+
+    var closeButton = document.createElement('button')
+    var closeIcon = document.createElement('span')
+    closeIcon.className = "fa fa-chevron-left"
+    closeButton.appendChild(closeIcon)
+    closeButton.className = "btn btn-primary"
+    closeButton.id = "collapseButton"
+    closeButton.onclick = () => {
+        close()
+    }
+    sidebar.appendChild(closeButton);
 }
 
-var closed = () => {
+var displayClosed = () => {
     var ul = document.createElement('ul')
     ul.className = "nav nav-pills flex-column";
 
@@ -52,18 +64,33 @@ var closed = () => {
         ul.appendChild(li)
         sidebar.appendChild(ul)
     })
+    var openButton = document.createElement('button')
+    var openIcon = document.createElement('span')
+    openIcon.className = "fa fa-chevron-right"
+    openButton.appendChild(openIcon)
+    openButton.className = "btn btn-primary"
+    openButton.id = "collapseButton"
+    openButton.onclick = () => {
+        open()
+    }
+    sidebar.appendChild(openButton);
 }
 
 var open = () => {
     sidebar.className = "col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar"
-    opened()
+    main.className = "col-sm-9 col-md-10 offset-sm-3 offset-md-2"
+    sidebar.innerHTML = null
+    displayOpened()
 }
 
 var close = () => {
-    sidebar.className = "col-sm-1 col-md-1 d-none d-sm-block bg-light sidebar"
-    closed()
+    sidebar.className = "col-sm-1 d-none d-sm-block bg-light sidebar collapsed"
+    main.className = "col-11 offset-1 collapsed"
+    sidebar.innerHTML = null
+    displayClosed()
 }
 
-close()
+// close()
+open()
 
 
