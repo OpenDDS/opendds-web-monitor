ab406908539309d30d09b95845026ee6762c4ebc
Brad Francis <bfranci@siue.edu>
2017-12-10 18:14:32 -0600

    created graph-view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GraphViewComponent } from './graph-view/graph-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphViewComponent
   ],
   imports: [
     BrowserModule
