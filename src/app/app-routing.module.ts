6157ca1e56efad779456ba77429d09ed82d4e85c
Brad Francis <bfranci@siue.edu>
2017-12-10 18:33:44 -0600

    Added route and import for component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProcessListComponent } from './process-list/process-list.component';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component'
+import { GraphViewComponent } from './graph-view/graph-view.component'
 
 
 const routes: Routes = [
   { path: '', component: HomepageComponent, pathMatch: 'full' },
-  { path: 'process-list', component: ProcessListComponent, pathMatch: "full" }
+  { path: 'process-list', component: ProcessListComponent, pathMatch: "full" }, 
+  { path: 'grap-hview', component: GraphViewComponent, pathMatch: "full" }
 ];
 
 @NgModule({
