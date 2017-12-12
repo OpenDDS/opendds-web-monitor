b1aa09151abf198560f4a03c5a080068380a1da5
Mitchell Dzurisin <madlab96@gmail.com>
2017-12-12 17:42:21 -0600

    Squashed commit of the following:
    commit 954c81537e2062fabd6ab6b02ef070476119747e
    Merge: 7df3efe e6d3138
    Author: Mitchell Dzurisin <madlab96@gmail.com>
    Date:   Tue Dec 12 17:38:16 2017 -0600
    
        Merge branch 'InitProcessList' of https://openproject.cs.siue.edu/fall-2018/git/ps-monitor into InitProcessList
    
    commit 7df3efeb13e171c2502944892e16998b6910a65b
    Author: Mitchell Dzurisin <madlab96@gmail.com>
    Date:   Tue Dec 12 17:23:27 2017 -0600
    
        Completed Port to angular
    
    commit b21993bd55a81e11f638cfbdb785de4beaa3e3d2
    Author: Mitchell Dzurisin <madlab96@gmail.com>
    Date:   Sun Dec 10 18:45:18 2017 -0600
    
        Updated process list routes
    
    commit e6d3138465714f1c8c0ebcc3909b5849c9d856fd
    Author: Mitchell Dzurisin <madlab96@gmail.com>
    Date:   Tue Dec 12 17:23:27 2017 -0600
    
        Completed Port to angular
    
    commit 3f02e21d6a3615582b24df022b699247e225b97b
    Author: Mitchell Dzurisin <madlab96@gmail.com>
    Date:   Sun Dec 10 18:45:18 2017 -0600
    
        Updated process list routes

--- src/app/process-list/process-list.component.ts
+++ src/app/process-list/process-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import processes from './processes'
 
 @Component({
   selector: 'app-process-list',
   templateUrl: './process-list.component.html',
-  styleUrls: ['./process-list.component.css']
+  styleUrls: ['./process-list.component.css'],
 })
+
 export class ProcessListComponent implements OnInit {
+  processes: Array<object> = processes;
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
