e7775f69ca2139a7fad42a28f00debd451a98595
madlab96@gmail.com <madlab96@gmail.com>
2018-02-03 18:20:34 -0600

    added socket event for subscribed data.

--- server/ddsHelper.js
+++ server/ddsHelper.js
@@ -1,4 +1,5 @@
 var opendds = require('opendds');
+var events = require('../eventConfig');
 
 function DataReader() {
   this.factory = null
@@ -7,26 +8,30 @@ function DataReader() {
   this.topic = null
 }
 
-DataReader.prototype.subscribe = function() {
+DataReader.prototype.subscribe = function(io) {
   var qos = {     
     DataReaderQos: { durability: "TRANSIENT_LOCAL_DURABILITY_QOS" }
   }
+  //Subscribe to any publishers with defined tags (second param)
   this.topic = this.participant.subscribe(
       'Valve',
       'Nexmatix::ValveData',
       qos,
       function(dr, sinfo, sample) {
         console.log(sample);
+        io.emit(events.subscribe, sample);
       }
   )
 }
 
 DataReader.prototype.initializeDR = function() {
   this.factory = opendds.initialize("-DCPSConfigFile ./rtps_disc.ini");
+  //load pre-compiled library
   this.library = opendds.load("lib/libNexmatix");
   if (!this.library) {
     throw new Error("Could not open support library, make sure idl is compile/path is correct.");
   }
+  //initialize participant with opendds module
   this.participant = this.factory.create_participant(23);
 }
 
--- server/main.js
+++ server/main.js
@@ -3,14 +3,14 @@ const port = 3333;
 var io = require('socket.io').listen(port);
 var events = require('../eventConfig');
 
+//connection socket event
 io.on(events.connection, function(socket) {
   var address = socket.handshake.address;
   console.log('New connection from ' + address.address + ':' + address.port);
 });
 
+//DataReader initialization 
 var DataReader = new DataReader();
-// Split out DDS args
-
 DataReader.initializeDR();
-
-DataReader.subscribe();
+//subscribe and send over the socket already created
+DataReader.subscribe(io);
