2337a30ba2aabfe0e15b65147079d2bb8235b2e4
Brad Francis <bfranci@siue.edu>
2018-02-10 17:35:53 -0600

    Imported gridster

--- monitor/src/app/homepage/homepage.component.spec.ts
+++ monitor/src/app/homepage/homepage.component.spec.ts
@@ -2,6 +2,42 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomepageComponent } from './homepage.component';
 
+import { GridsterConfig, GridsterItem }  from './angular-gridster2';
+   options: GridsterConfig;
+   dashboard: Array<GridsterItem>;
+ 
+   static itemChange(item, itemComponent) {
+     console.info('itemChanged', item, itemComponent);
+   }
+ 
+   static itemResize(item, itemComponent) {
+     console.info('itemResized', item, itemComponent);
+   }
+ 
+   ngOnInit() {
+     this.options = {
+       itemChangeCallback: AppComponent.itemChange,
+       itemResizeCallback: AppComponent.itemResize,
+     };
+ 
+     this.dashboard = [
+       {cols: 2, rows: 1, y: 0, x: 0},
+       {cols: 2, rows: 2, y: 0, x: 2}
+     ];
+   }
+ 
+   changedOptions() {
+     this.options.api.optionsChanged();
+   }
+ 
+   removeItem(item) {
+     this.dashboard.splice(this.dashboard.indexOf(item), 1);
+   }
+ 
+   addItem() {
+     this.dashboard.push({});
+   }
+
 describe('HomepageComponent', () => {
   let component: HomepageComponent;
   let fixture: ComponentFixture<HomepageComponent>;
--- monitor/src/app/homepage/homepage.component.ts
+++ monitor/src/app/homepage/homepage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
