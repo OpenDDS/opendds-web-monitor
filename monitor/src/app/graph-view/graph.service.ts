354e7c59eb41921ecd7e708080ad8cb77f4b1443
Brad Francis <bfranci@siue.edu>
2018-03-27 15:09:24 -0500

    working on graph/openddsbridge integration

--- monitor/src/app/graph-view/graph.service.ts
+++ monitor/src/app/graph-view/graph.service.ts
@@ -20,17 +20,85 @@ export class GraphService {
 
     loadGraph() {
 
-        //TODO reimplement with openddsBridge integration
-        const newNodes: GraphElement[] = [
-            new GraphElement('T 1', 50, 50, 'topic'), 
-            new GraphElement('R 1', 300, 300, 'reader'),
-            new GraphElement('W 1', 400, 160, 'writer')
-        ], 
-        newPaths: Path[] = [
-            new Path(50, 50, 300, 300),
-            new Path(50, 50, 400, 160)
-        ];
+        let topics = [], subscribers = [], publishers = [];
 
+        console.log(this.openddsBridge.data.publisher);
+
+        // load topics
+        this.openddsBridge.data.topic.map(topic => {
+            topics.push({name: topic.name, id: topic.topic_id});
+        })
+
+        // load pubs/writers
+        this.openddsBridge.data.publisher.map(pub => {
+            publishers.push({topic_id: pub.topic_id, writers: pub.writers});
+        })
+
+        // load subs/readers
+        this.openddsBridge.data.subscriber.map(sub => {
+            subscribers.push({topic_id: sub.topic_id, writers: sub.readers});
+        })
+
+        // determine connections
+        for (let topic of topics) {
+
+            // init conenctions
+            topic.connections = [];
+
+            // connect publishers
+            for (let pub of publishers) {
+                if (topic.id === pub.topic_id)
+                    topic.connections.push(pub);
+            }
+
+            // connect subscribers
+            for (let sub of subscribers) {
+                if (topic.id === sub.topic_id)
+                topic.connections.push(sub);
+            }
+        }
+
+        // generate elements
+        const newNodes: GraphElement[] = [];
+        const newPaths: Path[] = [];
+
+        let xOffset = 0, yOffset = 0; // properly spaces elements on graph
+        for (let topic of topics) {
+
+            let topicX = xOffset * 10;
+            let topicY = 100;
+
+            newNodes.push(new GraphElement(topic.name, xOffset * 100, 100, 'topic'))
+
+            for (let node of topic.conenctions) {
+
+                let nodeX = xOffset * 90;
+                let nodeY = 150;
+
+                // if subscriber
+                if (node.readers) {
+                    for (let reader of node.readers) {
+                        newNodes.push(new GraphElement('Reader', xOffset * 90, 150, 'reader'));
+                        newPaths.push(new Path(topicX, topicY, nodeX, nodeY));
+                    }
+                }
+
+                // if publisher
+                else if (node.writers) {
+                    for (let reader of node.readers) {
+                        newNodes.push(new GraphElement('Reader', xOffset * 90, 150, 'reader'));
+                        newPaths.push(new Path(topicX, topicY, nodeX, nodeY));
+                    }
+                }
+
+            }
+
+            // update offsets
+            xOffset++;
+            yOffset++;
+        }
+
+        // push nodes
         this.nodes.next(newNodes);
         this.paths.next(newPaths);
     }
